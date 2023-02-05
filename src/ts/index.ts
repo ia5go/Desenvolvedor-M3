ba9a69d11becbc0163760823fca0724d25a948a3
ia5go <benvindoiago@outlook.com>
2023-02-04 23:34:52 -0300

    Layout
    Layout quase finalizado, falta apenas um acerto dos sumarios para add o icones de seta para baixo;

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,32 @@ import { Product } from "./Product";
 
 const serverUrl = "http://localhost:5000";
 
+const SidebarsConfig = () => {
+  const mobileTriggers : NodeListOf<Element> = document.querySelectorAll('.mobile-triggers__item');
+
+  mobileTriggers.forEach((el : HTMLElement) =>{
+    const sidebar = document.getElementById(`${el.dataset.target}`); 
+    if(sidebar){
+      //block for building the  
+      const closeBtn = document.createElement('button');
+      closeBtn.classList.add('mobile-sidebar__close-btn');
+      closeBtn.innerHTML = "X";
+      sidebar.appendChild(closeBtn);
+  
+      el.addEventListener('click', () => {
+        sidebar.classList.add('open');
+      });
+      
+      closeBtn.addEventListener('click', () => {
+        sidebar.classList.remove('open');
+      });
+    }
+  });
+}
+
 function main() {
   console.log(serverUrl);
+  SidebarsConfig();
 }
 
 document.addEventListener("DOMContentLoaded", main);
